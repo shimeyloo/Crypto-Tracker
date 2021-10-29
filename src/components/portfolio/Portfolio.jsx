8103e088c5d6d07cc123ea38acdf356c8419695d
Master Shimey <shimeyloo@gmail.com>
2021-10-29 09:48:58 -0700

    Reorganized component tree

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
 import './Portfolio.css';
-import brokenIMG from '../images/brokenIMG.png' 
+import CoinRow from './CoinRow';
 
 // Notes: 
 // https://www.youtube.com/watch?v=dYjdzpZv5yc
@@ -9,56 +8,8 @@ import brokenIMG from '../images/brokenIMG.png'
 // const records = [{key: "Bitcoin", value: {"quantity": '12'}}, {key: "Ethereum", value: {"quantity": '543'}}]
 const records = []
 
-function CoinRow(props) {
+function SetTotal(props) {
   
-  const [name, setName] = useState([""]); 
-  const [logo, setLogo] = useState([brokenIMG]); 
-  const [symbol, setSymbol] = useState([""]); 
-  const [price, setPrice] = useState([""]); 
-  const [total, setTotal] = useState([""]); 
-
-  useEffect(() => {
-
-    axios
-      .get(
-        'https://api.coingecko.com/api/v3/coins/'+props.name.toLowerCase()+'?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false'
-      )
-      .then(res => {;
-        console.log(res.data);
-        setName(res.data.name);
-        setLogo(res.data.image.thumb); 
-        setSymbol(res.data.symbol.toUpperCase());
-        setPrice(res.data.market_data.current_price.usd);
-        
-        const totalCalc = function totalCalculation() {
-          let x = Number(res.data.market_data.current_price.usd)
-          let y = Number(props.amount)
-          let z = x * y 
-          props.setTotalFunction(z)
-          return z.toLocaleString()
-        }
-        setTotal(totalCalc)
-      })
-      .catch(error => console.log(error));
-  }, []);
-  
-  return(
-    <tr>
-      <th scope="row">{props.num}</th>
-      <td>
-        <img className="coin-logo" src={logo} alt="Logo" />
-        {name}
-      </td>
-      <td>{symbol}</td>
-      <td>{props.amount}</td>
-      <td>$ {price}</td>
-      <td>$ {total}</td>
-      <td>
-        <button>Delete</button>
-        <button>Edit</button>
-      </td>
-    </tr>
-  )
 }
 
 function Table() {
@@ -88,15 +39,6 @@ function Table() {
 
   // ------------------ Portfolio Total Value ------------------ //
   const [totalValue, setTotalValue] = useState(0); 
-
-  function handleTotalValue(tempValue) {
-    setTotalValue(prevItems => {
-      let sumVal = totalValue + tempValue
-      let roundtwoPlaces = Math.round(sumVal * 100) / 100
-      return roundtwoPlaces; 
-    }); 
-  }
-  console.log(typeof totalValue)
 
   return (
     <div>
@@ -153,7 +95,6 @@ function Table() {
                   num={index+1} 
                   name={value.key} 
                   amount={value.value.quantity} 
-                  setTotalFunction={handleTotalValue}
                 />
               ); 
             })}
